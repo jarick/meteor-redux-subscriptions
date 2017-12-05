0c9c17fc132903e99f647d07daebd3067d9c35b7
jarick <zolotarev.jar@gmail.com>
2017-12-06 01:23:18 +0200

    fix dublicate subscriptions in state

--- src/reducer.js
+++ src/reducer.js
@@ -10,13 +10,24 @@ export const SetSubscriptionActionType = '@METEOR_REDUX_SUBSCRIPTIONS/SUBSCRIBE'
 export const RemoveSubscriptionActionType = '@METEOR_REDUX_SUBSCRIPTIONS/REMOVE'
 type Action = SetSubscriptionAction | RemoveSubscriptionAction
 
-export default (state: SubscriptionsState = [], action: Action) => {
-  switch (action.type) {
-    case SetSubscriptionActionType:
-      return [ ...state, { ...action.payload } ]
-    case RemoveSubscriptionActionType:
-      return state.filter(({ id }) => id !== action.payload.id)
-    default:
-      return state
-  }
+const reducers = {};
+
+reducers[SetSubscriptionActionType] = (
+  state: SubscriptionsState,
+  action: SetSubscriptionAction
+): DataSetState<*> => {
+  return state.filter(({ id }) => id !== action.payload.id).concat({ ...action.payload })
+}
+
+reducers[RemoveSubscriptionActionType] = (
+  state: SubscriptionsState,
+  action: RemoveSubscriptionActionType
+): DataSetState<*> => {
+  return state.filter(({ id }) => id !== action.payload.id)
+}
+
+export default (state: DataSetState<*> = {}, action: Action): DataSetState<*> => {
+  const reducer = reducers[action.type]
+
+  return reducer ? reducer(state, (action: any)) : state
 }
